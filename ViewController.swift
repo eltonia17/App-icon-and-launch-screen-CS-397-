88fb59231e70861656dd4f3b1f68b8173d3bf7ee
eltonia17 <esl107@miami.edu>
2025-09-05 18:57:36 -0400

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aura
+//
+//  Created by Eltonia Leonard on 9/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
